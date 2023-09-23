029258068e3dbbe60e4dbd07707fa7364596baea
licikook1999 <lisethafri@gmail.com>
2023-09-22 19:42:27 -0500

    Primer commit

---
+++ src/app/api/auth/singup/route.ts
@@ -0,0 +1,16 @@
+import {NextResponse} from 'next/server'
+
+export async function POST(request: Request) {
+
+    const {fullname, email, password} = await request.json()
+    console.log(fullname, email, password)
+
+    if (!password || password.length < 6) return NextResponse.json({
+        message: "Password must be at least 6 characters"
+    }, {
+        status: 400
+    })
+    
+
+    return NextResponse.json({message: "signup"})
+}
